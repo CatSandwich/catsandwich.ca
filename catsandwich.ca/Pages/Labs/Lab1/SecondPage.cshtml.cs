c7cc4267b3f39ed4cd4829f401639462da28f98e
CatSandwich <59626141+CatSandwich@users.noreply.github.com>
2020-10-06 18:04:51 -0400

    Add nav bar and Lab1

---
+++ catsandwich.ca/Pages/Labs/Lab1/SecondPage.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace catsandwich.ca.Pages.Labs.Lab1
+{
+    public class SecondPageModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ catsandwich.ca/Pages/Labs/Lab2/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace catsandwich.ca.Pages.Labs
+{
+    public class Lab2Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
